44beeaee6a93d15fa53547b1599d8719136dce59
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-09-15 12:19:05 +0300

    modified file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+subtraction = number1 - number2
+product = number1 * number 2
+
+print(f"sum:{sum},subtraction:{subtraction},product:{product}")
+
