ea66dd5a0efb9629002adc6b87e60d039b0684fc
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-09-15 12:23:56 +0300

    modified file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,11 @@
+# global variables
 number1 = 10
 number2 = 5
 
+# operations
 sum = number1 + number2
 subtraction = number1 - number2
-product = number1 * number 2
+product = number1 * number2
 
+# output
 print(f"sum:{sum},subtraction:{subtraction},product:{product}")
-
