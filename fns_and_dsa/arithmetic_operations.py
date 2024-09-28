f136ca162bfcabbd6a0cef0d51987c7ad8c48f66
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-09-28 19:27:44 +0300

    modified file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,7 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
-    elif operation == 'divide' and num2 != 0:
+    elif operation == 'divide' and num2 == 0:
         return num1 / num2
         # else:
         # print("Number not divisible by zero!")
