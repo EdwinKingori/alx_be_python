28c857ef3aaa1b2e0f73c780046048efff93a84b
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-09-28 19:26:53 +0300

    modified file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,10 +5,9 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
-    elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
-            print("Number not divisible by zero!")
+    elif operation == 'divide' and num2 != 0:
+        return num1 / num2
+        # else:
+        # print("Number not divisible by zero!")
     else:
         return "Operation provided not recognized! Available operations include: add, subract, multiply and divide"
