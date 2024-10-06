b2980954a7de2b85bf4c68e1d6fe8ac07efea5f1
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-06 21:11:02 +0300

    Modify

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,12 +16,12 @@ class BankAccount:
                 # print(f"Withdrawn amount is: ${amount:.2f}")
                 # print(f"Your balance is {self.__account_balance:.2f}")
 
-            # else:
-            #     print("Insufficient funds.")
-            #     return False
-        else:
-            print("Insufficient funds.")
-            return False
+            else:
+                print("Insufficient funds.")
+                return False
+        # else:
+        #     print("Insufficient funds.")
+        #     return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.__account_balance:.2f}")
