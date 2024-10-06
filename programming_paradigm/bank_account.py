6e01f4f6c929a258e46d919edb4b7e8eb8b5b208
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-06 22:38:15 +0300

    modified

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,19 +9,16 @@ class BankAccount:
             return f"Deposited: ${amount:.1f}"
 
     def withdraw(self, amount):
-        if amount > 0:
-            if amount <= self.__account_balance:
-                self.__account_balance -= amount
-                return f"Withdrew: ${amount:.1f}"
-                # print(f"Withdrawn amount is: ${amount:.2f}")
-                # print(f"Your balance is {self.__account_balance:.2f}")
 
-            else:
-                print("Insufficient funds.")
-                return False
-        # else:
-        #     print("Insufficient funds.")
-        #     return False
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return f"Withdrew: ${amount:.1f}"
+            # print(f"Withdrawn amount is: ${amount:.2f}")
+            # print(f"Your balance is {self.__account_balance:.2f}")
+
+        else:
+            print("Insufficient funds.")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.__account_balance:.2f}")
