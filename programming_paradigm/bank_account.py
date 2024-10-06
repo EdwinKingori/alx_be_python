09976932f115e95e79f87961a31975d915652b57
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-06 20:31:42 +0300

    class logic

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,41 @@
+# class definition
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"You deposited ${amount}")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.__account_balance:
+                self.__account_balance -= amount
+                print(f"Withdrawn amount is: ${amount}")
+                print(f"Your balance is {self.__account_balance}")
+                return True
+            else:
+                print("insufficient funds to conduct this withdrawal")
+                return False
+        else:
+            print("Insufficient funds to withdraw.")
+            return False
+
+    def display_balance(self):
+        return f"The current blance in your account is: ${self.__account_balance}"
+
+
+account = BankAccount(2000)
+print(account.display_balance())
+
+account.deposit(500)
+print(account.display_balance())
+
+success_withdraw = account.withdraw(70)
+print(success_withdraw)
+account.display_balance()
+
+failure = account.withdraw(200)
+print(failure)
+account.display_balance()
