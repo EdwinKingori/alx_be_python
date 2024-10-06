de34ad394adca4aa482f53b6448e23764a0f3e2e
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-06 21:06:21 +0300

    Modified

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,7 +12,7 @@ class BankAccount:
         if amount > 0:
             if amount <= self.__account_balance:
                 self.__account_balance -= amount
-                print(f"Withdrew: ${amount:.2f}")
+                print(f"Withdrew: ${amount:.1f}")
                 # print(f"Withdrawn amount is: ${amount:.2f}")
                 # print(f"Your balance is {self.__account_balance:.2f}")
                 return True
