10607045f974d184c6bd390eac530b491ecca167
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-06 20:36:30 +0300

    Modified File

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,7 +23,7 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        return f"Current balance: ${self.__account_balance}"
+        print(f"Current balance: ${self.__account_balance}")
 
 
 account = BankAccount(2000)
