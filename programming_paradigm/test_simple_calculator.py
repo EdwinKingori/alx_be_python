696139cb1700b17fef78ae0dcac0f67ce0e7a619
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-07 13:56:29 +0300

    add class imported

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,6 +4,9 @@ from simple_calculator import SimpleCalculator
 
 class TestCalculator(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
     def test_add(self):
         self.assertEqual(self.calc.add(2, 3), 5)
 
