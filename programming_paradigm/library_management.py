3f42df96e1870020fe7cca13519ff0be3b7cd4a2
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-07 14:25:10 +0300

    library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,58 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def __str__(self):
+        return f"Book(title='{self.title}', author='{self.author}')"
+
+    def check_out(self):
+        self.__is_checked_out = True
+
+    def return_book(self):
+        return self.__is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if not book.is_checked_out():
+                    book.check_out()
+                    print(f"Checked out: {book.title}")
+                    return True
+                else:
+                    print(f"Book '{book.title}' is already checked out.")
+                    return False
+        print(f"Book '{title}' not found in the library.")
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.is_checked_out():
+                    book.return_book()
+                    print(f"Returned: {book.title}")
+                    return True
+                else:
+                    print(f"Book '{book.title}' was not checked out.")
+                    return False
+        print(f"Book '{title}' not found in the library.")
+        return False
+
+    def list_available_books(self):
+        available_books = [
+            book for book in self._books if not book.is_checked_out()]
+        if available_books:
+            print("Available books:")
+            for book in available_books:
+                print(f"- {book.title} by {book.author}")
+        else:
+            print("No books available.")
