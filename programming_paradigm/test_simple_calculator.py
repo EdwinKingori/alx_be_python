947fc0bf10ea7d7d7583de4ee914b978db604de2
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-07 13:58:46 +0300

    Modified cases

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,13 +11,13 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
 
     def test_substraction(self):
-        self.assertEqual(self.calc.add(6, 3), 3)
+        self.assertEqual(self.calc.subtract(6, 3), 3)
 
     def test_multiplication(self):
-        self.assertEqual(self.calc.add(2, 3), 6)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
 
     def test_division(self):
-        self.assertEqual(self.calc.add(10, 5), 2)
+        self.assertEqual(self.calc.divide(10, 5), 2)
 
 
 if __name__ == '__main__':
